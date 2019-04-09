9908ef2891a86e8837c38cc504792d1ba2c4dad2
wjh2363301965 <2363301965@qq.com>
2019-04-09 21:00:21 +0800

    冒泡排序

--- buttlesort/src/BubbleSort.java
+++ buttlesort/src/BubbleSort.java
@@ -3,11 +3,16 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] arr={12,23,34,56,56,56,78,23,224,2,1,4,5};
-        bubbleSort(arr);
+      /*  bubbleSort(arr);
 
         for (int i : arr) {
             System.out.println(i);
+        }*/
+        bubbleSort2(arr);
+        for (int i : arr) {
+            System.out.println(i);
         }
+
     }
     //这是百度上的
     public static void bubbleSort(int[] arr){
@@ -27,21 +32,21 @@ public class BubbleSort {
 
     }
     //理解后我自己又做了一下，虽然没有百度的代码简单
-    int a=0;
-    int   b;
-    public  void bubbleSort2(int[] arr){
+    static int a=0;
+    static int   b;
+    public static void bubbleSort2(int[] arr){
 
         for (int i = 0;i<arr.length-1;i++){
             for (int j = 0;j<arr.length-1-a;j++){
 
-               if (arr[i]>arr[i+1]){
-                   b=arr[i+1];
-                   arr[i+1]=arr[i];
-                   arr[i]=b;
+               if (arr[j]>arr[j+1]){
+                   b=arr[j+1];
+                   arr[j+1]=arr[j];
+                   arr[j]=b;
                }
 
             }
-
+            a++;
         }
     }
 
